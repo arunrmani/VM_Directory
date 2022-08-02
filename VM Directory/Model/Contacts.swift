39d0de5a08d2addfdcc2abb1de73b68ab784c5e9
aicenter-ios <arunrmani@aicenter.ae>
2022-08-02 10:42:32 +0400

    Data Model For Contact And Rooms

---
+++ VM Directory/Model/Contacts.swift
@@ -0,0 +1,20 @@
+//
+//  Contacts.swift
+//  VM Directory
+//
+//  Created by Safe City Mac 001 on 02/08/2022.
+//
+
+import Foundation
+
+
+struct Contacts: Codable{
+    var createdAt: String?
+    var firstName: String?
+    var avatar: String?
+    var lastName: String?
+    var email: String?
+    var jobtitle: String?
+    var favouriteColor: String?
+    var id: String?
+}
---
+++ VM Directory/Model/Rooms.swift
@@ -0,0 +1,15 @@
+//
+//  Rooms.swift
+//  VM Directory
+//
+//  Created by Safe City Mac 001 on 02/08/2022.
+//
+
+import Foundation
+
+struct Rooms: Codable{
+    var createdAt: String?
+    var isOccupied: String?
+    var maxOccupancy: String?
+    var id: String?
+}
