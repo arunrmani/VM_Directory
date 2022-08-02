0d23db87fa2a4d2324e25dfc29588935a09faf37
aicenter-ios <arunrmani@aicenter.ae>
2022-08-02 15:55:07 +0400

    Added sample quotes

---
+++ VM Directory/Model/Quotes.swift
@@ -0,0 +1,13 @@
+//
+//  Quotes.swift
+//  VM Directory
+//
+//  Created by Safe City Mac 001 on 02/08/2022.
+//
+
+import Foundation
+
+struct Quotes:Codable{
+    var quote: String?
+    var author: String?
+}
